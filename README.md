4e2b9f4395f9505c363dc015676aee17eb5be95e
joeldevelops <joeldevelops@nanoapi.io>
2024-10-27 21:33:10 +0100

    Initial commit with example repo

---
+++ README.md
@@ -0,0 +1,27 @@
+# NodeJS Example APIs
+
+This repo contains a collection of NodeJS APIs that showcase the functionality of [NanoAPI's CLI](https://github.com/nanoapi-io/napi).
+
+## Quick Start
+
+Ensure you have `napi` installed:
+
+```bash
+npm install -g @nanoapi.io/napi
+```
+
+Clone this repo and navigate to the desired framework/API directory:
+
+```bash
+git clone https://github.com/nanoapi-io/nodejs-example-apis.git && \
+cd nodejs-example-apis/express/1-simple && \
+npm i
+```
+
+You're all set to run `napi` commands!
+
+```bash
+napi init
+napi ui
+```
+
---
+++ express/basic/index.js
@@ -0,0 +1,63 @@
+import express from 'express';
+
+const cats = [
+  { id: 1, name: 'Fluffy' },
+  { id: 2, name: 'Whiskers' },
+  { id: 3, name: 'Tiger' },
+];
+
+const dogs = [
+  { id: 1, name: 'Rex' },
+  { id: 2, name: 'Spot' },
+  { id: 3, name: 'Buddy' },
+];
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// @nanoapi method:GET path:/index
+app.get('/index', (req, res) => {
+  res.send('Hello World!');
+});
+
+// @nanoapi method:GET path:/api/v0/cats group:cats
+app.get('/api/v0/cats', async (req, res) => {
+  res.send(cats);
+});
+
+// @nanoapi method:GET path:/api/v0/cats/:id group:cats
+app.get('/api/v0/cats/:id', async (req, res) => {
+  const cat = cats.find((cat) => cat.id === parseInt(req.params.id));
+  res.send(cat);
+});
+
+// @nanoapi method:POST path:/api/v0/cats group:cats
+app.post('/api/v0/cats', async (req, res) => {
+  const cat = req.body;
+  cats.push(cat);
+  res.send(cat);
+});
+
+// @nanoapi method:GET path:/api/v1/dogs group:dogs
+app.get('/api/v1/dogs', async (req, res) => {
+  res.send(dogs);
+});
+
+// @nanoapi method:GET path:/api/v1/dogs/:id group:dogs
+app.get('/api/v1/dogs/:id', async (req, res) => {
+  const dog = dogs.find((dog) => dog.id === parseInt(req.params.id));
+  res.send(dog);
+});
+
+// @nanoapi method:POST path:/api/v1/dogs group:dogs
+app.post('/api/v1/dogs', async (req, res) => {
+  const dog = req.body;
+  dogs.push(dog);
+  res.send(dog);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
